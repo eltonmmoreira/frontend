04b64f90665d45916813cea22d8888b0ef854447
eltonmmoreira <eltonmmoreira@gmail.com>
2020-11-02 18:09:29 -0300

    Validações

--- src/app/pessoa/pessoa.form.component.ts
+++ src/app/pessoa/pessoa.form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Pessoa} from './pessoa';
 import {PessoaService} from './pessoa.service';
 import {MessageService} from 'primeng/api';
@@ -6,6 +6,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {Location} from '@angular/common';
 import {environment} from '../../environments/environment';
 import {UrlUtil} from '../util/urlUtil';
+import {FormControl, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-pessoa-form',
@@ -14,6 +15,7 @@ import {UrlUtil} from '../util/urlUtil';
 })
 export class PessoaFormComponent implements OnInit {
 
+  @ViewChild(NgForm, {static: false}) public form: NgForm;
   pessoa: Pessoa = new Pessoa();
   urlUpload: string;
   baseUrl = `${environment.url}/file/upload/pessoa/`;
@@ -30,11 +32,23 @@ export class PessoaFormComponent implements OnInit {
   }
 
   novo(): void {
+    this.inicializarForm();
     this.pessoa = new Pessoa();
     const url = UrlUtil.urlContainsForm(this.router.url)
       ? UrlUtil.getAtForm(this.router.url)
       : this.router.url;
     this.location.go(url);
+  }
+
+  inicializarForm(): void {
+    const form = this.form;
+    for (const eachControl in form.controls) {
+      (<FormControl>form.controls[eachControl]).reset();
+      (<FormControl>form.controls[eachControl]).markAsUntouched();
+      (<FormControl>form.controls[eachControl]).markAsPristine();
+      (<FormControl>form.controls[eachControl]).updateValueAndValidity();
+    }
+    form.resetForm();
   }
 
   init(): void {
@@ -60,17 +74,31 @@ export class PessoaFormComponent implements OnInit {
   }
 
   salvar(): void {
+    if (!this.validarForm()) {
+      return;
+    }
     this.pessoaService.save(this.pessoa)
       .subscribe(pessoa => {
         const id = this.pessoa.id;
         this.pessoa = pessoa;
         this.messageService.add({severity: 'success', detail: 'Registro salvo com sucesso!'});
-
         if (!id) {
           this.atualizarUrlUpload();
           this.location.go(UrlUtil.getAtForm(this.router.url) + '/' + this.pessoa.id);
         }
       });
+  }
+
+  validarForm(): boolean {
+    if (!this.form.valid) {
+      const form = this.form;
+      for (const eachControl in form.controls) {
+        (<FormControl>form.controls[eachControl]).markAsDirty();
+        (<FormControl>form.controls[eachControl]).updateValueAndValidity();
+      }
+      return false;
+    }
+    return true;
   }
 
   onUpload(event): void {
